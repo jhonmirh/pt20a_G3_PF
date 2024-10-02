45e83fbd6c3aab51063fdf5d8a2ee0edb766295d
jhonmirh <130907039+jhonmirh@users.noreply.github.com>
2024-10-02 13:26:40 -0400

    back con cotegories.categoryId => product=>categoryId

--- front/src/components/Aside/Aside.tsx
+++ front/src/components/Aside/Aside.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import AlertModal from "../Alert/AlertModal";
 import CardsPropsAside from "./types";
 import { useLoggin } from "@/context/logginContext";
 import { useRouter } from "next/navigation";
-import Image from 'next/image';
+import Image from "next/image";
 
 interface CardPropsAside extends CardsPropsAside {
   id: string;
@@ -12,40 +12,42 @@ interface CardPropsAside extends CardsPropsAside {
   imageUrl?: string;
   hoverImageUrl?: string;
   description?: string;
-  categoryId:string;
+  categoryId: string;
 }
 
-
-
-const AsideBar: React.FC<CardPropsAside> = ({ id, name, imageUrl, hoverImageUrl, description, categoryId }) => {
+const AsideBar: React.FC<CardPropsAside> = ({
+  id,
+  name,
+  imageUrl,
+  hoverImageUrl,
+  description,
+  categoryId,
+}) => {
   const [showModal, setShowModal] = useState(false);
   const [redirectPath, setRedirectPath] = useState<string | null>(null);
   const { userData } = useLoggin();
   const router = useRouter();
-  const [isHovered, setIsHovered] = useState(false); // Estado para manejar el hover
-  const APIURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3010';
-  
- 
+  const [isHovered, setIsHovered] = useState(false);
+  const APIURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3010";
+
   const handleLinkClick = async (categoryId: string) => {
     console.log(`Fetching products for categoryId: ${categoryId}`);
     try {
-      const response = await fetch(`${APIURL}/categories/${categoryId}/products`); // Asegúrate de que APIURL esté apuntando al puerto 3010
+      const response = await fetch(
+        `${APIURL}/categories/${categoryId}/products`
+      );
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
       console.log(data);
- 
+
       router.push(`/categories/${categoryId}`);
     } catch (error) {
-      console.error('Error fetching products:', error);
+      console.error("Error fetching products:", error);
     }
   };
-  
 
-
-  
- 
   const handleModalClose = () => {
     setShowModal(false);
     if (redirectPath) {
@@ -55,36 +57,39 @@ const AsideBar: React.FC<CardPropsAside> = ({ id, name, imageUrl, hoverImageUrl,
 
   return (
     <div
-      className={`flex flex-col justify-center items-center ${isHovered ? "bg-gray-900" : "bg-white"} border border-blue-500 rounded-lg shadow-lg shadow-blue-500/50 cursor-pointer mb-4 p-4 transition duration-300 ease-in-out`}
-      style={{ width: isHovered ? "320px" : "180px", height: "220px" }} 
+      className={`flex flex-col justify-center items-center ${
+        isHovered ? "bg-gray-900" : "bg-white"
+      } border border-blue-500 rounded-lg shadow-lg shadow-blue-500/50 cursor-pointer mb-4 p-4 transition duration-300 ease-in-out`}
+      style={{ width: isHovered ? "320px" : "180px", height: "220px" }}
       onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)} 
+      onMouseLeave={() => setIsHovered(false)}
       onClick={() => handleLinkClick(categoryId)}
-
-      
     >
       <div className="flex flex-col items-start p-4">
         {isHovered ? (
-          
           <div className="flex flex-row items-center">
-          <Image
-            src={hoverImageUrl || '/default-image.jpg'} 
-            alt={name}
-            width={80}
-            height={80}
-            className="ml-4" 
-          />
-          <p className={`text-white ml-8 mb-5`}>{description}</p>
-        </div>
+            <Image
+              src={hoverImageUrl || "/default-image.jpg"}
+              alt={name}
+              width={80}
+              height={80}
+              className="ml-4"
+            />
+            <p className={`text-white ml-8 mb-5`}>{description}</p>
+          </div>
         ) : (
           <Image
-            src={imageUrl || '/default-image.jpg'}
+            src={imageUrl || "/default-image.jpg"}
             alt={name}
             width={80}
             height={80}
           />
         )}
-        <h5 className={`text-base font-bold tracking-tight ${isHovered ? "text-white" : "text-blue-700"}`}>
+        <h5
+          className={`text-base font-bold tracking-tight ${
+            isHovered ? "text-white" : "text-blue-700"
+          }`}
+        >
           {name}
         </h5>
         {/* {description && (
