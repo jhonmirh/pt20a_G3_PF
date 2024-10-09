5c29b66ff55a473f29f525c2673a2df35d49ecd1
PMoyanoAiraldi <pmoyano497@gmail.com>
2024-10-08 23:50:07 -0300

    UPDATE IN MODAL COMPONENT COMPLETE y auth3ro

--- front/src/context/logginContext.tsx
+++ front/src/context/logginContext.tsx
@@ -7,6 +7,7 @@ import IProduct from "@/interfaces/Products";
 export interface LogginContextProps {
   userData: userSession | null;
   setUserData: (userData: userSession | null) => void;
+  loginWithGoogle: (googleUserData: { credential: string }) => Promise<{ user: any, token: string }>
   selectedProduct: IProduct | null;
   setSelectedProduct: (product: IProduct | null) => void;
 }
@@ -15,6 +16,9 @@ export interface LogginContextProps {
 export const LogginContext = createContext<LogginContextProps>({
   userData: null,
   setUserData: () => {},
+  loginWithGoogle: async () => {
+    return { user: {}, token: "" };
+  },
   selectedProduct: null,
   setSelectedProduct: () => {},
 });
@@ -27,6 +31,37 @@ export interface LogginProviderProps {
 export const LogginProvider: React.FC<LogginProviderProps> = ({ children }) => {
   const [userData, setUserData] = useState<userSession | null>(null);
   const [selectedProduct, setSelectedProduct] = useState<IProduct | null>(null);
+
+// Función de inicio de sesión con Google
+const loginWithGoogle = async (googleUserData: { credential: string }) : Promise<{ user: any, token: string }>=> {
+  try {
+    const response = await fetch(`http://localhost:3010/auth/google-login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ token: googleUserData.credential }),
+      
+    });
+    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    const userData = await response.json();
+    console.log("Response data:", userData);
+    const newUser = {
+      token: userData.token,
+      user: {
+        name: userData.userData.name || "Sin datos", // O asigna un valor predeterminado si falta
+        email: userData.userData.email || "Sin datos",
+        phone: userData.userData.phone || "Sin datos",
+      },
+    };
+    console.log("Formatted User Data:", newUser);
+    return newUser;
+  } catch (error) {
+    console.error("Error al iniciar sesión con Google:", error);
+    throw error; // Lanza el error para que se maneje en handleGoogleLoginSuccess
+  }
+};
 
   // Efecto para almacenar datos de usuario en localStorage cuando cambian
   useEffect(() => {
@@ -56,6 +91,7 @@ export const LogginProvider: React.FC<LogginProviderProps> = ({ children }) => {
       value={{
         userData,
         setUserData,
+        loginWithGoogle,
         selectedProduct,
         setSelectedProduct,
       }}
