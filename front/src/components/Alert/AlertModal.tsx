93ae805c893b6b0f0095bb27b76887e1a6950dd9
jhonmirh <130907039+jhonmirh@users.noreply.github.com>
2024-10-17 19:49:39 -0400

    version 0.121

--- front/src/components/Alert/AlertModal.tsx
+++ front/src/components/Alert/AlertModal.tsx
@@ -14,7 +14,7 @@ const AlertModal: React.FC<AlertModalProps> = ({ showModal, handleClose, title, 
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-md w-full text-center">
         <div className="text-4xl mb-4 text-red-600">
-          🚨
+        
         </div>
         <h2 className="text-xl font-bold mb-4 text-gray-900 dark:text-gray-100">
           {title}
