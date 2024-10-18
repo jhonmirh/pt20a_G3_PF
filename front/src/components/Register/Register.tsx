dd5c1d0cf408f9240e40c26632b0d172c344c8df
jhonmirh <130907039+jhonmirh@users.noreply.github.com>
2024-10-17 21:03:25 -0400

    version 0.124

--- front/src/components/Register/Register.tsx
+++ front/src/components/Register/Register.tsx
@@ -4,12 +4,81 @@ import { registerUser } from "@/helpers/auth.helper";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import AlertModal from "../Alert/AlertModal";
-import { validateRegisterLogin } from "@/helpers/validators";
 
-export const dynamic = 'force-dynamic';
+// Validation functions
+export function validatePassword(password: string): string | undefined {
+    if (password && !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)) {
+        return "El password debe contener al menos un carácter especial, un número, una letra mayúscula y una letra minúscula y tener al menos ocho caracteres.";
+    }
+    return undefined; 
+}
+
+export function validateLogin(values: ILogin): ILoginError {
+    const errors: ILoginError = {};
+    if (!values.email) {
+        errors.email = "Email es Requerido.";
+    } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(values.email)) {
+        errors.email = "Invalid email address.";
+    }
+    return errors;
+}
+
+export function validateRegisterLogin(values: IRegister): TRegisterError {
+    const errors: TRegisterError = {};
+    
+    if (!values.email) {
+        errors.email = "Email es Requerido.";
+    } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(values.email)) {
+        errors.email = "Invalid email address.";
+    }
+
+    if (!values.password) {
+        errors.password = "Password es Requerido.";
+    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/.test(values.password)) {
+        errors.password = "La contraseña debe contener al menos un carácter especial, un número, una letra mayúscula y una letra minúscula y tener al menos ocho caracteres.";
+    }
+
+    if (!values.passwordConfirm) {
+        errors.passwordConfirm = "Password es Requerido.";
+    } else if (values.password !== values.passwordConfirm) {
+        errors.passwordConfirm = "Password no Coinciden.";
+    }
+
+    if (!values.name) {
+        errors.name = "Nombre es Requerido.";
+    } else if (!/^[a-zA-Z\s]{3,30}$/.test(values.name)) {
+        errors.name = "El nombre debe contener entre 3 y 30 caracteres.";
+    }
+
+    if (values.phone === undefined) {
+        errors.phone = "Número Telefónico es Requerido.";
+    } else if (typeof values.phone !== 'number' || !/^\d{10}$/.test(values.phone.toString())) {
+        errors.phone = "El número de teléfono debe ser numérico y tener 10 dígitos.";
+    }
+
+    if (values.age === undefined || values.age === null) {
+        errors.age = "Edad es Requerido.";
+    } else if (typeof values.age !== 'number' || values.age < 18 || values.age > 99 || !/^\d{2}$/.test(values.age.toString())) {
+        errors.age = "La edad debe ser un número de dos dígitos mayor o igual a 18";
+    }
+
+    if (!values.city) {
+        errors.city = "Ciuda es Requerido.";
+    } else if (!/^[a-zA-Z]{3,30}$/.test(values.city)) {
+        errors.city = "La ciudad debe contener sólo letras y tener entre 3 y 30 caracteres.";
+    }
+
+    if (!values.address) {
+        errors.address = "Dirección es Requerido.";
+    } else if (!/^[a-zA-Z0-9\s\.,_-áéíóúÁÉÍÓÚñÑ]{3,30}$/.test(values.address)) {
+        errors.address = "La dirección debe contener entre 3 y 30 caracteres";
+    }
+
+    return errors;
+}
+
+
 const Register = () => {
-
-
   const startState: IRegister = {
     name: "",
     email: "",
@@ -83,21 +152,19 @@ const Register = () => {
 
     try {
       const response = await registerUser(dataUser);
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
-      console.log("Usuario Registrado:", response);
+      console.log("Usuario Registrado Ingresa con tus Datos:", response);
       setModalContent({
         title: "Éxito",
-        message: "Usuario registrado satisfactoriamente.",
+        message: "Usuario registrado satisfactoriamente. Por favor, inicia sesión.",
       });
       setShowModal(true);
+      setTimeout(() => {
+        handleCloseModal();
+      }, 5000);
     } catch (error: any) {
-      console.error("Error durante el registro:", error);
-      setModalContent({
-        title: "Error",
-        message: error.message || "Se produjo un error inesperado.",
-      });
+      console.error(error);
+      const errorMessage = error.message || "Se produjo un error inesperado durante el registro.";
+      setModalContent({ title: "Error", message: errorMessage });
       setShowModal(true);
     }
   };
