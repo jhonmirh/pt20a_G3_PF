a8d7604cdb0f178fee6917246bab16a5ba2f31bc
jhonmirh <130907039+jhonmirh@users.noreply.github.com>
2024-10-17 20:34:42 -0400

    version 0.123 Titulo, Botones Pagar Modales sin logo

--- front/src/components/NavBar/NavBar.tsx
+++ front/src/components/NavBar/NavBar.tsx
@@ -28,7 +28,7 @@ export default function NavBar({ images }: NavBarProps) {
   };
   const handleLogoClick = (e: React.MouseEvent) => {
     e.preventDefault(); // Previene el comportamiento predeterminado de <Link>
-    
+
     if (!isProfileComplete()) {
       // Si el perfil no está completo, redirige a /completar-perfil
       router.push("/completar-perfil");
@@ -58,103 +58,79 @@ export default function NavBar({ images }: NavBarProps) {
           <Image src="/logo-JhonDay3.png" alt="Logo" width={50} height={50} />
         </Link>
 
-        <div className="relative flex items-center w-full max-w-md">
-          {/* <input
-            type="text"
-            placeholder="Search..."
-            className="w-full p-2 pr-10 rounded border border-gray-700 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && handleSearch()}
-          /> */}
-          <h1 className="text-white">SERVICIOS TECNOLÓGICOS INTEGRALES JHONDAY</h1>
-          {/* <button
-            onClick={handleSearch}
-            className="absolute right-1 top-1/2 transform -translate-y-1/2 p-1 rounded hover:bg-blue-500"
-            title="Search"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-4.35-4.35m2.15-5.4a7.5 7.5 0 11-15 0 7.5 7.5 0 0115 0z"
-              />
-            </svg>
-          </button> */}
+        <div className="flex items-center w-full max-w-md justify-center">
+          <h1 className="text-white text-2xl md:text-3xl font-bold whitespace-nowrap relative">
+            SERVICIOS TECNOLÓGICOS INTEGRALES JHONDAY
+            <span className="absolute inset-0 text-gray-500 blur-[2px] translate-x-1 translate-y-1">
+              SERVICIOS TECNOLÓGICOS INTEGRALES JHONDAY
+            </span>
+          </h1>
         </div>
 
         <nav className="flex items-center space-x-6">
-    
-    
-    {userData?.userData.admin &&
-          <Menu as="div" className="relative inline-block text-left">
-            <div>
-              <Menu.Button className="flex items-center text-white hover:text-blue-500 transition-colors duration-300">
-                Admin
-                <ChevronDownIcon className="w-5 h-5 ml-1" aria-hidden="true" />
-              </Menu.Button>
-            </div>
-            <Transition
-              as={React.Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right bg-gray-800 border border-gray-700 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
-                <div className="px-1 py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/dashboardAdmin/appointments"
-                        className={`${
-                          active ? "bg-blue-500 text-white" : "text-white"
-                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                      >
-                        Turnos
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/dashboardAdmin/categories"
-                        className={`${
-                          active ? "bg-blue-500 text-white" : "text-white"
-                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                      >
-                        Servicios
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/dashboardAdmin/users"
-                        className={`${
-                          active ? "bg-blue-500 text-white" : "text-white"
-                        } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                      >
-                        Usuarios Registrados
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </div>
-              </Menu.Items>
-            </Transition>
-          </Menu>
-}
-
-
+          {userData?.userData.admin && (
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="flex items-center text-white hover:text-blue-500 transition-colors duration-300">
+                  Admin
+                  <ChevronDownIcon
+                    className="w-5 h-5 ml-1"
+                    aria-hidden="true"
+                  />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={React.Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right bg-gray-800 border border-gray-700 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/dashboardAdmin/appointments"
+                          className={`${
+                            active ? "bg-blue-500 text-white" : "text-white"
+                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        >
+                          Turnos
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/dashboardAdmin/categories"
+                          className={`${
+                            active ? "bg-blue-500 text-white" : "text-white"
+                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        >
+                          Servicios
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/dashboardAdmin/users"
+                          className={`${
+                            active ? "bg-blue-500 text-white" : "text-white"
+                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        >
+                          Usuarios Registrados
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          )}
 
           {userData ? (
             <>
